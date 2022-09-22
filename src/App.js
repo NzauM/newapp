dbe2607a1672e7e09547547a30b6d4e9a6c1be85
mercy.waeni <mercy.waeni@calltronix.com>
2022-09-22 11:18:05 +0300

    add event listeners and handlers and introduce state

--- src/App.js
+++ src/App.js
@@ -3,20 +3,30 @@ import './App.css';
 import Button from './Button';
 import Quotes from './Quotes';
 import { myQuotes } from './Data';
+import { useState } from 'react';
 
 function App() {
+  const[myState, setmyState] = useState(0)
+  console.log("App Componet is called once")   
   let myList = myQuotes.map((elem, ind)=>{
-    console.log(elem)
-    console.log(ind)
+    // console.log(elem)
+    // console.log(ind)
     return(
       <Quotes key={ind} quoteheading={elem.quoteheading} quoteparagraph={elem.quoteparagraph} quotebutton={elem.quotebutton}/>
     )
   })
+  // console.log(myState)
+
+  function handlepClick(){
+    console.log('p is clicked')
+    setmyState((myState)=>myState+=1)
+  }
+  // console.log(myState)
   
   return (
    <>
     <h1 style={{color: 'red'}}>Karibu Phase 2</h1>
-    <p>This should be interesting</p>
+    <p onClick={handlepClick}>This should be interesting</p>
     <Button/>
     {myList}
    </>
--- src/Quotes.js
+++ src/Quotes.js
@@ -1,10 +1,44 @@
+import { useState } from "react"
+
 function Quotes({quoteheading="This quote does not have an heading", quoteparagraph="So we decided to give it that lame one", quotebutton="Buy an headingless quote?"}){
-    // console.log(props)
+    // const[state, stateSetter] = useState(initialvalue)
+    const[username, setUsername] = useState("")
+    const[hovered, setHovered] =  useState(false)
+    const[varname, fnChangevarName] = useState('what varname will initially be')
+
+    // fnChangevarName("new value of varname")
+    // console.log(varname)
+    // fnChangevarName((varname)=>'new value of varname')
+
+    console.log("Quotes component rendered")
+
+    
+    function handlePriceClick(){
+        alert(`${quoteheading} has been added to your cart`)
+    }
+    console.log(hovered)
+
+    function changeColor(){
+        console.log("After hover")
+        setHovered((hovered)=>!hovered)
+    }
+
+    console.log(hovered)
+
+
+    function handleSubmit(e){
+        let user_input = e.target.value
+        console.log(user_input)
+    }
+
     return(
         <>
-            <h1>{quoteheading}</h1>
+            <h1 className={hovered?'App-link': 'catherine'} onMouseEnter={changeColor}>{quoteheading}</h1>
             <p>{quoteparagraph}</p>
-            <button>{quotebutton}</button>
+            <button onClick={handlePriceClick}>{quotebutton}</button>
+            <label>Write here</label>
+            <input onBlur={handleSubmit} type='text' name="username"></input>
+
         </>
     )
 }
